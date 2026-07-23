34c2d55de51c51f8d057d6d03d4f64275ffa5b78
ExDeDe_ <szymon78545_3@proton.me>
2026-07-23 02:38:18 +0200

    Port to Minecraft 26.1

---
+++ versions/26.1/src/main/java/com/bedrockobfuscator/Reference.java
@@ -0,0 +1,9 @@
+package com.bedrockobfuscator;
+
+/** Small holder for mod-wide constants. */
+public final class Reference {
+    public static final String MOD_ID = "bedrockobfuscator";
+    public static final String MOD_NAME = "Bedrock Obfuscator";
+
+    private Reference() {}
+}
